55489db160e0b462160912162f8a5d4329d87f7f
wdavilav <william@lezag.com>
2023-06-26 22:52:14 -0500

    Datatables - learning the columnDefs property

--- core/pos/static/product/js/list.js
+++ core/pos/static/product/js/list.js
@@ -25,6 +25,46 @@ var product = {
                 {data: 'pvp'},
                 {data: 'stock'},
                 {data: 'id'}
+            ],
+            // ordering: false,
+            columnDefs: [
+                {
+                    targets: [-5],
+                    class: 'text-center',
+                    render: function (data, type, row) {
+                        if (data) {
+                            return '<span class="badge bg-primary rounded-pill">Si</span>';
+                        }
+                        return '<span class="badge bg-info rounded-pill">No</span>';
+                    },
+                },
+                {
+                    targets: [-4, -3],
+                    class: 'text-center',
+                    render: function (data, type, row) {
+                        return '$' + data;
+                    },
+                },
+                {
+                    targets: [-2],
+                    class: 'text-center',
+                    render: function (data, type, row) {
+                        if (data < 5) {
+                            return '<span class="badge bg-danger rounded-pill">' + data + '</span>';
+                        }
+                        return '<span class="badge bg-success rounded-pill">' + data + '</span>';
+                    },
+                },
+                {
+                    targets: [-1],
+                    class: 'text-center',
+                    orderable: false,
+                    render: function (data, type, row) {
+                        var buttons = '<a href="' + pathname + 'update/' + row.id + '/" data-bs-toggle="tooltip" title="Editar" class="btn btn-warning btn-sm"><i class="fas fa-edit"></i></a> ';
+                        buttons += '<a href="' + pathname + 'delete/' + row.id + '/" data-bs-toggle="tooltip" title="Eliminar" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></a>';
+                        return buttons;
+                    }
+                },
             ]
         });
     }
